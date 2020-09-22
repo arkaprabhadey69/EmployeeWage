7688f16c1063a0ca1529bbb79770d15aac1e3598
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 18:41:09 +0530

    [Arka]Added:Attendance

--- Employee.java
+++ Employee.java
@@ -3,5 +3,16 @@ public class Employee{
 public static void main(String[] args) {
 	
 	System.out.println("Welcome to EmployeeWage");
+	int IS_FULL_TIME=1;
+
+	double empcheck= Math.floor(Math.random()*10)%2;
+
+	if(empcheck==IS_FULL_TIME)
+	{
+		System.out.println("Employee present");
+	}
+	else
+		System.out.println("Employee absent");
 }
+
 }
