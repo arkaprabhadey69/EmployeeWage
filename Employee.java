843fe5cc8167a7609266e0c1ba09d2f133c231a6
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 18:54:39 +0530

    [Arka]Added:Monthly restriction

--- Employee.java
+++ Employee.java
@@ -2,6 +2,7 @@ public class Employee{
 	static final int IS_FULL_TIME=2;
 	static final int EMP_RATE_HR=20;
 	static final int IS_PART_TIME=1;
+	static final int NUM_WORKING_DAYS=20;
 
 	
 public static void main(String[] args) {
@@ -10,6 +11,9 @@ public static void main(String[] args) {
 
 	int employeewage=0;
 	int employeehrs=0;
+	int totalwage=0;
+	for(int day=0;day<NUM_WORKING_DAYS;day++)
+{
 
 	int empcheck=(int) Math.floor(Math.random()*10)%3;
 
@@ -25,9 +29,15 @@ public static void main(String[] args) {
 		employeehrs=0;
 	}
 
-	employeewage=EMP_RATE_HR*employeehrs;
 
-	System.out.println("Emp wage is: "+employeewage);
+	employeewage=EMP_RATE_HR*employeehrs;
+	totalwage=totalwage+employeewage;
+	System.out.println("Emp wage for day "+day+" is: "+employeewage);
+
+
+}
+
+	System.out.println("Total emp wage is: "+totalwage);
 }
 
 }
