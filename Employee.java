4f6e86a36aa87559db7c55d0e3bdbeb9f76e5613
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 18:45:07 +0530

    [Arka]Added: Daily wage

--- Employee.java
+++ Employee.java
@@ -1,18 +1,25 @@
 public class Employee{
 	
 public static void main(String[] args) {
-	
+
 	System.out.println("Welcome to EmployeeWage");
+	
 	int IS_FULL_TIME=1;
+	int EMP_RATE_HR=20;
+
+	int employeewage=0;
+	int employeehrs=0;
 
 	double empcheck= Math.floor(Math.random()*10)%2;
 
 	if(empcheck==IS_FULL_TIME)
 	{
-		System.out.println("Employee present");
+		employeehrs=8;
 	}
-	else
-		System.out.println("Employee absent");
+	
+	employeewage=EMP_RATE_HR*employeehrs;
+
+	System.out.println("Emp wage is: "+employeewage);
 }
 
 }
